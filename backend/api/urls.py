31d090c7ed1e4c20871f4f8c1fde7a4dcc5f2d47
Adrian <adrian.skrzypnik11@gmail.com>
2025-03-29 20:13:48 +0100

    register login working, TODO error handling, input validation

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,8 +3,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 urlpatterns = [
-    path('me/', api.me, name='me'),
-    path('register/', api.register, name='register'),
+    path('me/', views.me, name='me'),
+    path('register/', views.register, name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from .forms import SignupForm
+from rest_framework.permissions import AllowAny
 
 @api_view(['GET'])
 def me(request):
@@ -11,19 +12,17 @@ def me(request):
         'is_staff': request.user.is_staff,
     })
 
-
 @api_view(['POST'])
 @authentication_classes([])
 @permission_classes([])
 def register(request):
     data = request.data
+    print(data)
     message = 'success'
 
     form = SignupForm({
         'email': data.get('email'),
         'name': data.get('name'),
-        'position': data.get('position'),
-        'birth_date': data.get('birth_date'),
         'password1': data.get('password1'),
         'password2': data.get('password2'),
     })
@@ -32,6 +31,7 @@ def register(request):
         form.save()
 
     else:
+        print(form.errors)
         message = 'error'
 
     return JsonResponse({'message':message})
